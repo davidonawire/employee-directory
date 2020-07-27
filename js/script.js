044155d7335e2e480a409a7c9a324ece38d9b858
David Adams <onethinline@gmail.com>
2020-07-27 14:04:27 -0700

    Add initial fetch logic

--- js/script.js
+++ js/script.js
@@ -3,4 +3,20 @@
  * 
  */
 
- 
+const requestURL = 'https://randomuser.me/api/';
+const employeeCount = 12;
+let employees = [];
+
+
+async function getEmployees(url) {
+    try {
+        const response = await fetch(url);
+        return await response.json();
+    } catch (error) {
+        throw error;
+    }
+}
+
+getEmployees(requestURL + `?results=${employeeCount}`)
+    .then(response => employees = response.results)
+    .catch(e => console.error('Error in fetch:', e));
