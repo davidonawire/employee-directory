92148edc42d6f9bcdcc806933f880bbcdf96ae42
David Adams <onethinline@gmail.com>
2020-07-27 15:32:55 -0700

    Add gallery creation

--- js/script.js
+++ js/script.js
@@ -6,9 +6,10 @@
 const requestURL = 'https://randomuser.me/api/';
 const employeeCount = 12;
 let employees = [];
+const gallery = document.getElementById('gallery');
 
 
-async function getEmployees(url) {
+async function getJSON(url) {
     try {
         const response = await fetch(url);
         return await response.json();
@@ -17,6 +18,33 @@ async function getEmployees(url) {
     }
 }
 
-getEmployees(requestURL + `?results=${employeeCount}`)
+getJSON(requestURL + `?results=${employeeCount}`)
     .then(response => employees = response.results)
     .catch(e => console.error('Error in fetch:', e));
+
+function createEmployeeCard(employee) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const cardImg = document.createElement('div');
+    cardImg.classList.add('card-img-container');
+
+    const cardInfo = document.createElement('div');
+    cardInfo.classList.add('card-info-container');
+
+    card.appendChild(cardImg);
+    card.appendChild(cardInfo);
+
+    cardImg.innerHTML = `<img class="card-img" src="${employee.picture.large}" alt="profile picture">`;
+    cardInfo.innerHTML = `<h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
+                        <p class="card-text">${employee.email}</p>
+                        <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>`
+
+    return card;
+}
+
+function createGallery() {
+    employees.forEach(employee => {
+        gallery.appendChild( createEmployeeCard(employee) );
+    });
+}
